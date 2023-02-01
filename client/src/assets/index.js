71bfb42cba44bdf5989c616bd7e78aa5e2a8a5f7
Nischal Dahal <neeswebservice@gmail.com>
2023-02-02 04:05:03 +0545

    cleared frontend

---
+++ client/src/assets/index.js
@@ -0,0 +1,11 @@
+const ASSETS = {
+  images: {
+    logoDark: require("./images/dark.png"),
+    appIcon: require("./images/icon.png"),
+  },
+  colors: {
+    primary: "#292929",
+  },
+};
+
+export default ASSETS;
