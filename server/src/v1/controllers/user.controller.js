db99ff561bf42f3b25d65bde694b67aa8adf0c07
Nischal Dahal <neeswebservice@gmail.com>
2023-02-04 12:59:33 +0545

    created user controller

---
+++ server/src/v1/controllers/user.controller.js
@@ -0,0 +1,7 @@
+export const registerUser = (req, res, next) => {
+  res.send("register here");
+};
+
+export const loginUser = (req, res, next) => {
+  res.send("login here");
+};
--- server/src/v1/routes/users.js
+++ server/src/v1/routes/users.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import { loginUser, registerUser } from "../controllers/user.controller";
 const router = Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
+
+router.route("/login").post(loginUser);
+router.route("/register").post(registerUser);
 
 export default router;
